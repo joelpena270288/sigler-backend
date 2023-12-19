3bd0db9ef232c40955b2a9b0fb93ae25f3e0a766
joelpena270288 <joelpenagonzalez122@gmail.com>
2023-12-19 14:43:20 -0400

    quitar hasroles

--- src/modules/pago-anticipados/pago-anticipados.controller.ts
+++ src/modules/pago-anticipados/pago-anticipados.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PagoAnticipadosService } from './pago-anticipados.service';
 import { CreatePagoAnticipadoDto } from './dto/create-pago-anticipado.dto';
 import { UpdatePagoAnticipadoDto } from './dto/update-pago-anticipado.dto';
-import { HasRoles, HasRoles } from '../role/roles.decorator';
+import { HasRoles} from '../role/roles.decorator';
 import { RoleEnum } from '../role/enums/role.enum';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../role/guards/roles.guard';
 
 @Controller('pago-anticipados')
 export class PagoAnticipadosController {
-  constructor(private readonly pagoAnticipadosService: PagoAnticipadosService) {}
-  @HasRoles(
-    RoleEnum.ADMIN,
-  
-  )
+  constructor(
+    private readonly pagoAnticipadosService: PagoAnticipadosService,
+  ) {}
+  @HasRoles(RoleEnum.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Post()
   create(@Body() createPagoAnticipadoDto: CreatePagoAnticipadoDto) {
@@ -41,19 +49,16 @@ export class PagoAnticipadosController {
   findOne(@Param('id') id: string) {
     return this.pagoAnticipadosService.findOne(+id);
   }
-  @HasRoles(
-    RoleEnum.ADMIN,
-  
-  )
+  @HasRoles(RoleEnum.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePagoAnticipadoDto: UpdatePagoAnticipadoDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePagoAnticipadoDto: UpdatePagoAnticipadoDto,
+  ) {
     return this.pagoAnticipadosService.update(+id, updatePagoAnticipadoDto);
   }
-  @HasRoles(
-    RoleEnum.ADMIN,
-  
-  )
+  @HasRoles(RoleEnum.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
