f9616148f036ec5c01f937de5676b6406849c886
joelpena270288 <joelpenagonzalez122@gmail.com>
2023-12-23 19:52:40 -0400

    cambiar cotizacion

--- src/modules/cotizacion/cotizacion.service.ts
+++ src/modules/cotizacion/cotizacion.service.ts
@@ -27,7 +27,7 @@ export class CotizacionService {
     const foundProyecto: Proyecto = await this.proyectoRepository.findOne({
       where: {
         id: createCotizacionDto.proyectoId,
-        status: StatusProyecto.CANCELADO,
+        status: Not( StatusProyecto.CANCELADO),
       },
     });
     if (!foundProyecto) {
