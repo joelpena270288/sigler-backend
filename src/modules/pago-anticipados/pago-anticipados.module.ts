4fef87217ac1ae0f76102e9b8c09f522de774f9e
joelpena270288 <joelpenagonzalez122@gmail.com>
2023-12-27 18:46:54 -0400

    add CuentaEmpresaProviders

--- src/modules/pago-anticipados/pago-anticipados.module.ts
+++ src/modules/pago-anticipados/pago-anticipados.module.ts
@@ -4,10 +4,11 @@ import { PagoAnticipadosController } from './pago-anticipados.controller';
 import { DatabaseModule } from '../../database/database.module';
 import { PagoAnticipadoProviders } from './pago-anticipados.providers';
 import { ClienteProviders } from '../cliente/cliente.providers';
+import { CuentaEmpresaProviders } from '../cuentas-empresa/cuentas-empresa.providers';
 @Module({
   imports: [DatabaseModule],
   controllers: [PagoAnticipadosController],
-  providers: [PagoAnticipadosService, ...PagoAnticipadoProviders,...ClienteProviders],
+  providers: [PagoAnticipadosService, ...PagoAnticipadoProviders,...ClienteProviders,...CuentaEmpresaProviders],
   exports:[PagoAnticipadosService]
 })
 export class PagoAnticipadosModule {}
