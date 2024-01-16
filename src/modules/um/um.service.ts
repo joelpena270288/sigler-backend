a35ff8fcb512b76fdb78a1a6ccb2ca86f6d7bc09
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-01-16 18:07:29 -0400

    actualizar import

--- src/modules/um/um.service.ts
+++ src/modules/um/um.service.ts
@@ -1,44 +1,44 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUmDto } from './dto/create-um.dto';
 import { UpdateUmDto } from './dto/update-um.dto';
 import { Um } from './entities/um.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UmService {
   constructor(
     @Inject('UM_REPOSITORY')
-    private umRepository: Repository<Um>
-  
+    private umRepository: Repository<Um>,
   ) {}
- async create(createUmDto: CreateUmDto): Promise<Um> {
-   const um: Um = new Um();
-   um.name = createUmDto.name;
-   return await this.umRepository.save(um);
+  async create(createUmDto: CreateUmDto): Promise<Um> {
+    const um: Um = new Um();
+    um.name = createUmDto.name;
+    return await this.umRepository.save(um);
   }
 
-async  findAll():Promise<Um[]> {
+  async findAll(): Promise<Um[]> {
     return await this.umRepository.find();
   }
 
- async findOne(id: string): Promise<Um> {
-    return await this.umRepository.findOne({where: {id: id}});
+  async findOne(id: string): Promise<Um> {
+    return await this.umRepository.findOne({ where: { id: id } });
   }
 
- async update(id: string, updateUmDto: UpdateUmDto) {
-   const foundUm: Um = await this.umRepository.findOne({where:{id: id}});
-   if(!foundUm){
-    return NotFoundException("El id introducido no es valido");
-   }
-   foundUm.name = updateUmDto.name;
-   foundUm.updatedAt = new Date();
-   return await this.umRepository.save(foundUm);
+  async update(id: string, updateUmDto: UpdateUmDto) {
+    const foundUm: Um = await this.umRepository.findOne({ where: { id: id } });
+    if (!foundUm) {
+      return NotFoundException('El id introducido no es valido');
+    }
+    foundUm.name = updateUmDto.name;
+    foundUm.updatedAt = new Date();
+    return await this.umRepository.save(foundUm);
   }
 
- async remove(id: string):Promise<Um> {
-  const foundUm: Um = await this.umRepository.findOne({where:{id: id}});
-  if(!foundUm){
-   return NotFoundException("El id introducido no es valido");
-  }
- return await this.umRepository.remove(foundUm);
+  async remove(id: string): Promise<Um> {
+    const foundUm: Um = await this.umRepository.findOne({ where: { id: id } });
+    if (!foundUm) {
+      return NotFoundException('El id introducido no es valido');
+    }
+    return await this.umRepository.remove(foundUm);
   }
 }
