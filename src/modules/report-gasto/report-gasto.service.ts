fac905b33660e2ebddc34c5a0ef8521c058821b7
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-01-18 19:42:15 -0400

    arreglar reporte gasto

--- src/modules/report-gasto/report-gasto.service.ts
+++ src/modules/report-gasto/report-gasto.service.ts
@@ -2,10 +2,11 @@ import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { ReadReportGastoDto } from './dto/read-report-gasto.dto';
 import { GastosEmpresa } from '../gastos_empresas/entities/gastos_empresa.entity';
 import { ConsumoCombustible } from '../consumo_combustible/entities/consumo_combustible.entity';
-import {FiltroFechaDto}  from './dto/filtro-fecha.dto';
+import { FiltroFechaDto } from './dto/filtro-fecha.dto';
 import { Status } from '../../EntityStatus/entity.estatus.enum';
 import { Repository } from 'typeorm';
 import { EntradaCombustible } from '../entrada-combustible/entities/entrada-combustible.entity';
+import { StatusGasto } from '../gastos_empresas/entities/gasto-status.enum';
 @Injectable()
 export class ReportGastoService {
   constructor(
@@ -13,14 +14,9 @@ export class ReportGastoService {
     private gastoempresaRepository: Repository<GastosEmpresa>,
     @Inject('ENTRADACOMBUSTIBLE_REPOSITORY')
     private entradacombustibleRepository: Repository<EntradaCombustible>,
-   
   ) {}
 
- async getReport(filtroFechaDto: FiltroFechaDto): Promise<ReadReportGastoDto> {
-  if(filtroFechaDto.start ==null|| filtroFechaDto.end ==null){
-    throw new BadRequestException('Debe introducir una fecha inicio y una fecha fin');
-
-  }
+  async getReport(filtroFechaDto: FiltroFechaDto): Promise<ReadReportGastoDto> {
     const readReportGastoDto: ReadReportGastoDto = new ReadReportGastoDto();
     readReportGastoDto.itbis_gasto_combustible = 0;
     readReportGastoDto.itbis_gasto_empresa = 0;
@@ -28,51 +24,73 @@ export class ReportGastoService {
     readReportGastoDto.sub_total_gasto_combustible = 0;
     readReportGastoDto.sub_total_gasto_empresa = 0;
     readReportGastoDto.sub_total_gasto_proyecto = 0;
-  const gastos: GastosEmpresa[] = await this.gastoempresaRepository.createQueryBuilder('gasto')
-  .leftJoinAndSelect('gasto.proyecto','proyecto')
-  .leftJoinAndSelect('gasto.gastosItems','gastosItems')
-  .where('gasto.createdAt >= :start',{start: filtroFechaDto.start+' 00:00:00'}) 
-  .andWhere('gasto.createdAt  <= :end',{end: filtroFechaDto.end+' 23:59:00'})
-  .andWhere('gasto.status =:status',{status:Status.ACTIVO})
-  .getMany();
+    const gastos: GastosEmpresa[] = await this.gastoempresaRepository
+      .createQueryBuilder('gasto')
+      .leftJoinAndSelect('gasto.proyecto', 'proyecto')
+      .leftJoinAndSelect('gasto.gastosItems', 'gastosItems')
+      .where('gasto.createdAt >= :start', {
+        start: filtroFechaDto.start + ' 00:00:00',
+      })
+      .andWhere('gasto.createdAt  <= :end', {
+        end: filtroFechaDto.end + ' 23:59:00',
+      })
+      .andWhere('gasto.status !=:status', { status: StatusGasto.CANCELADO })
+      .getMany();
 
-  const combustible: EntradaCombustible[] = await this.entradacombustibleRepository.createQueryBuilder('combustible')
-  .where('combustible.fecha >= :start',{start: filtroFechaDto.start+' 00:00:00'}) 
-  .andWhere('combustible.fecha  <= :end',{end: filtroFechaDto.end+' 23:59:00'})
-  .andWhere('combustible.status =:status',{status:Status.ACTIVO})
-  .getMany();
-  if(gastos){
-
-    for (let index = 0; index < gastos.length; index++) {
-     if(gastos[index].proyecto != null){
-      for (let iter = 0; iter < gastos[index].gastosItems.length; iter++) {
-        readReportGastoDto.itbis_gasto_proyecto  = parseFloat(readReportGastoDto.itbis_gasto_proyecto.toString()) + parseFloat(gastos[index].gastosItems[iter].importeimpuesto.toString());
-        readReportGastoDto.sub_total_gasto_proyecto = parseFloat( readReportGastoDto.sub_total_gasto_proyecto.toString())+ parseFloat(gastos[index].gastosItems[iter].importe.toString()); 
-        
+    const combustible: EntradaCombustible[] =
+      await this.entradacombustibleRepository
+        .createQueryBuilder('combustible')
+        .where('combustible.fecha >= :start', {
+          start: filtroFechaDto.start + ' 00:00:00',
+        })
+        .andWhere('combustible.fecha  <= :end', {
+          end: filtroFechaDto.end + ' 23:59:00',
+        })
+        .andWhere('combustible.status =:status', { status: Status.ACTIVO })
+        .getMany();
+    if (gastos) {
+      for (let index = 0; index < gastos.length; index++) {
+        if (gastos[index].proyecto != null) {
+          for (let iter = 0; iter < gastos[index].gastosItems.length; iter++) {
+            readReportGastoDto.itbis_gasto_proyecto =
+              parseFloat(readReportGastoDto.itbis_gasto_proyecto.toString()) +
+              parseFloat(
+                gastos[index].gastosItems[iter].importeimpuesto.toString(),
+              );
+            readReportGastoDto.sub_total_gasto_proyecto =
+              parseFloat(
+                readReportGastoDto.sub_total_gasto_proyecto.toString(),
+              ) +
+              parseFloat(gastos[index].gastosItems[iter].importe.toString());
+          }
+        } else {
+          for (let iter = 0; iter < gastos[index].gastosItems.length; iter++) {
+            readReportGastoDto.itbis_gasto_empresa =
+              parseFloat(readReportGastoDto.itbis_gasto_empresa.toString()) +
+              parseFloat(
+                gastos[index].gastosItems[iter].importeimpuesto.toString(),
+              );
+            readReportGastoDto.sub_total_gasto_empresa =
+              parseFloat(
+                readReportGastoDto.sub_total_gasto_empresa.toString(),
+              ) +
+              parseFloat(gastos[index].gastosItems[iter].importe.toString());
+          }
+        }
       }
-
-     }else{
-      for (let iter = 0; iter < gastos[index].gastosItems.length; iter++) {
-        readReportGastoDto.itbis_gasto_empresa  = parseFloat( readReportGastoDto.itbis_gasto_empresa.toString() ) + parseFloat(gastos[index].gastosItems[iter].importeimpuesto.toString());
-        readReportGastoDto.sub_total_gasto_empresa = parseFloat( readReportGastoDto.sub_total_gasto_empresa.toString())+ parseFloat(gastos[index].gastosItems[iter].importe.toString()); 
-        
-      }
-
-     }
-      
     }
-  }
-  if(combustible){
-  for (let index = 0; index < combustible.length; index++) {
-   readReportGastoDto.itbis_gasto_combustible = parseFloat(readReportGastoDto.itbis_gasto_combustible.toString() ) + parseFloat(combustible[index].importeimpuesto.toString());
-   readReportGastoDto.sub_total_gasto_combustible = parseFloat(readReportGastoDto.sub_total_gasto_combustible.toString() ) + parseFloat(combustible[index].importe.toString());
-  }
+    if (combustible) {
+      for (let index = 0; index < combustible.length; index++) {
+        readReportGastoDto.itbis_gasto_combustible =
+          parseFloat(readReportGastoDto.itbis_gasto_combustible.toString()) +
+          parseFloat(combustible[index].importeimpuesto.toString());
+        readReportGastoDto.sub_total_gasto_combustible =
+          parseFloat(
+            readReportGastoDto.sub_total_gasto_combustible.toString(),
+          ) + parseFloat(combustible[index].importe.toString());
+      }
+    }
 
+    return readReportGastoDto;
   }
-
-return readReportGastoDto;
- 
-  }
-
-
 }
