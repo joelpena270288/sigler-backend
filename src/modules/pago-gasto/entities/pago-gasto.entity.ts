506a6fe0adf913d7cdd54e0b5e5115ad70a6e3f6
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-02-05 20:14:58 -0400

    quitar fecha banco

--- src/modules/pago-gasto/entities/pago-gasto.entity.ts
+++ src/modules/pago-gasto/entities/pago-gasto.entity.ts
@@ -40,8 +40,7 @@ export class PagoGasto {
       cuenta: CuentasEmpresa; 
       @Column({ type: 'varchar', nullable: false, default: Status.ACTIVO })
 status: string;
-@CreateDateColumn({ type: 'timestamp', name: 'created_at', nullable: true })
-fechaBanco: Date;
+
 @CreateDateColumn({ type: 'timestamp', name: 'created_at', nullable: true })
 createdAt: Date;
 @CreateDateColumn({ type: 'timestamp', name: 'updated_at', nullable: true })
