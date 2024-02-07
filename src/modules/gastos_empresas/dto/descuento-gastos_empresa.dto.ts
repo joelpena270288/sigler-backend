8b402944f59c87242fae87b796348390755d9f67
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-02-07 12:53:47 -0400

    eliminar descuento

--- src/modules/gastos_empresas/dto/descuento-gastos_empresa.dto.ts
+++ src/modules/gastos_empresas/dto/descuento-gastos_empresa.dto.ts
@@ -2,7 +2,7 @@
 import { IsString, IsInt,IsDecimal, IsNotEmpty } from "class-validator";
 import {CreateGastoItemDto} from '../../gasto_item/dto/create-gasto_item.dto';
 export class DescuentoGastosEmpresaDto {
-    @IsNotEmpty()
+   
     descuento: string;  
    
     @IsDecimal()
