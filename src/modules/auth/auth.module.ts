823fb6c791e6bdbc061c0aed57fb7136cdac455b
joelpena270288 <joelpenagonzalez122@gmail.com>
2024-03-05 16:57:25 -0400

    poner 12 horas al token

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -21,7 +21,7 @@ import { jwtConstants } from './dto/JWT.consts';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '8h' },
+      signOptions: { expiresIn: '12h' },
     }),
   ],
   controllers: [AuthController],
